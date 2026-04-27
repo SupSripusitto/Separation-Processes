54cda6e4c1c0d43c2c3985a84df6dbd58ea0afa1
SupSripusitto <supsripusitto@gmail.com>
2026-04-27 16:18:39 +0700

    Add README.md and Absorber stage counting program

---
+++ AbsorberStages.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+''' User input part'''
+
+# Output display (1 for needed, 0 for not needed)
+graph = 1
+report = 1
+
+# Solute mole ratio (or approximately mole fraction) in absorbent feed
+X0 = 0
+
+# Gas feed solute mole ratio
+YN1 = 0.0067
+
+# Gas outlet spec. (put negative value for recovery of solute in absorbent)
+Y1 = -0.67
+
+# Molar gas (without solute) flow rate
+V = 1
+
+# Molar liquid (without solute) flow rate (put negative value for multiples of the minimum value)
+L = -1.5
+
+# Equilibrium constant/Partitioning coefficient (allow only for constant value)
+K = 67
+
+# Maximum number of stages (to prevent infinite loop calculation, no need to change the value from 150 stages, if it isn't necessary)
+Nm = 150
+
+''' The end of user inputs, the next part is only the algorithms.'''
+
+# Find operating line slope
+slope = None
+
+if L > 0:
+    slope = L/V
+elif Y1 > 0:
+    slope = -K*(YN1-Y1)/YN1*L
+else:
+    slope = K*Y1*L
+    Y1 = (1+Y1)*YN1
+
+# Find the ending criterion
+XN = X0 + (YN1-Y1)/slope
+
+XYonline = np.zeros((2*Nm,2))
+
+XYonline[0,:] = [X0,Y1]
+
+i = 0
+
+while XYonline[i,0] < XN:
+    Yn = XYonline[i,1]
+    i += 1
+    XYonline[i,:] = [Yn/K,Yn]
+    
+    Xn = XYonline[i,0]
+    i += 1
+    XYonline[i,:] = [Xn,Y1 + slope*(Xn-X0)]
+
+    if i == 2*Nm-2:
+        print("Too extreme condition, the number of stages is over the limit.")
+        break
+
+if report:
+    print("-=-=Calculation Report=-=-")
+    print("Number of stages:         ", i//2)
+    print("Outlet liquid mole raio:  ", XYonline[i,0])
+    print("Feed mole ratio capable:  ", XYonline[i,1])
+
+if graph:
+    plt.plot(XYonline[:i+1,0],XYonline[:i+1,1])
+    plt.plot([0,1.7*XN],[0,1.7*K*XN])
+    plt.plot([X0,XN],[Y1,YN1])
+    plt.show()
---
+++ README.md
@@ -0,0 +1,18 @@
+# Introduction
+I made these files of separation processes graphical calculation Python code for all my friends and my junior chemical engineering students at Chulalongkorn University (I'm the CU107 and ChE49). It's fine if you're not my friends or my juniors, since I intentionally made this for free education resources. Also, I've not seen anyone made this and published before in my department (or other university in Thailand); therefore, I made this to show an approach which is more convinient than MS Excel or hand calculation. This set of programs is free source and light-duty, so this enable you to run it on your potato computer, tablet, or even your phone.
+
+# Installation Guide for real low-tech engineer
+
+**If you can't do any of these steps, it's fine to copy my code to online python editor.**
+
+1. Download Python from https://www.python.org/downloads/
+2. **(Optional)** Download Python editor, there're 2 editor I recommended which are Thonny and Visual Studio Code. (For potato PC, I recommended not to download any of this the Python IDLE is enough.)
+3. Install necessary pakages which is NumPy and Matplotlib using pip install in your terminal (For Thonny, you might need to check in the editor, not in the terminal)
+4. Download Git from https://git-scm.com/
+5. Type cmd in your start menu to open terminal (Command Prompt), locate where you want to store the file with cd, clone this repository using git clone as the following commands
+~~~
+cd "Your directory path to store this file, use / instead of \"
+git clone https://github.com/SupSripusitto/Separation-Processes
+~~~
+
+*Let's enjoy with this subject.*
